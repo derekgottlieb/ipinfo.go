4ef251becb6e176c21a9dbe1cc136d3f738bd5f9
Derek Gottlieb <derek.gottlieb@gmail.com>
2025-03-17 16:18:03 -0500

    Switch port to 3000

--- main.go
+++ main.go
@@ -96,8 +96,8 @@ func main() {
 	http.HandleFunc("/", ipHandler)
 	http.HandleFunc("/details", detailsHandler)
 
-	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting server on :3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
